b709f471bea17df91c66333cc48e843ca9c41418
nlom0218 <nlom0218@naver.com>
2022-06-08 23:00:58 +0900

    TailwindCSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    // 아래의 경로에서 Tailwind를 사용할 것이라고 알림
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js.jsx,tx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
