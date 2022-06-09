adbf04a32e88ef7665d24737a7744c7cc115924d
nlom0218 <nlom0218@naver.com>
2022-06-10 00:03:29 +0900

    Test Drive part Three

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,9 +21,30 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
-      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
-      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
+      <div className="overflow-hidden rounded-3xl  bg-white shadow-xl">
+        <div className=" bg-blue-500 p-6 pb-14">
+          <span className=" text-2xl text-white">Profile</span>
+        </div>
+        <div className=" relative -top-5 rounded-3xl bg-white p-6">
+          <div className=" relative -top-16 flex items-end justify-between">
+            <div className=" flex flex-col items-center">
+              <span className=" text-sm text-gray-500">Orders</span>
+              <span className=" font-medium">340</span>
+            </div>
+            <div className=" h-24 w-24 rounded-full bg-red-400" />
+            <div className=" flex flex-col items-center">
+              <span className=" text-sm text-gray-500">Spent</span>
+              <span className=" font-medium">$2,310</span>
+            </div>
+          </div>
+          <div className="relative -mt-10 -mb-5 flex flex-col items-center">
+            <span className=" text-lg font-medium">Tony Molloy</span>
+            <span className=" text-sm text-gray-500">미국</span>
+          </div>
+        </div>
+      </div>
+      <div className="rounded-3xl bg-white p-10 shadow-xl"></div>
+      <div className="rounded-3xl bg-white p-10 shadow-xl"></div>
     </div>
   );
 };
