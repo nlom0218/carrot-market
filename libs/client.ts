90af9bdf09f41ee3fce7f5defae3ad4b276a8ee3
nlom0218 <nlom0218@naver.com>
2022-06-22 22:29:50 +0900

    API Routes

--- libs/client.ts
+++ libs/client.ts
@@ -1,10 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = new PrismaClient();
-
-client.user.create({
-  data: {
-    email: "nlom02182@naver.com",
-    name: "khd",
-  },
-});
+export default new PrismaClient();
