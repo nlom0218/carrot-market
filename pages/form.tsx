29866c068c40b5ecd4b33394247e9ff568e368a6
nlom0218 <nlom0218@naver.com>
2022-06-23 18:49:32 +0900

    Validation part Two

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { FieldError, useForm } from "react-hook-form";
 
 // Less code
 // Better validation
@@ -8,33 +8,46 @@ import { useForm } from "react-hook-form";
 // Dont deal with events
 // Easier Inputs
 
+interface FormTypes {
+  username: string;
+  password: string;
+  email: string;
+}
+
 export default function Form() {
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit } = useForm<FormTypes>({
     mode: "onChange",
   });
-  const onSubmit = (data) => {
+  const onSubmit = (data: FormTypes) => {
     console.log("im vaild bby");
+  };
+  const onInvaild = (errors: FieldError) => {
+    console.log(errors);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit, onInvaild)}>
       <input
         {...register("username", {
-          required: true,
+          required: "Username is required",
+          minLength: {
+            value: 5,
+            message: "The username should be longer than 5 chars.",
+          },
         })}
         type="text"
         placeholder="Username"
       />
       <input
         {...register("email", {
-          required: true,
+          required: "Email is required",
         })}
         type="email"
         placeholder="Email"
       />
       <input
         {...register("password", {
-          required: true,
+          required: "Password is required",
         })}
         type="password"
         placeholder="Password"
