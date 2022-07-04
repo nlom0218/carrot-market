4e63d9394afb41ed66f5c19de3c0ebff86cca217
nlom0218 <nlom0218@naver.com>
2022-07-04 23:45:57 +0900

    Serverless Sessions

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,6 +1,7 @@
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import { withIronSessionApiRoute } from "iron-session/next";
 
 async function handler(
   req: NextApiRequest,
@@ -10,9 +11,24 @@ async function handler(
     body: { token },
   } = req;
 
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+
+  if (!exists) res.status(404).end();
+
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
 
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password:
+    "sdsdfsdfs3d2f1s31df23s1df3s1df23sd1f23s1df23s1fd231s2df1s32d1f32s1df3213s2f",
+});
